9c95350b1ceed81549ec9a34be5d3cad99d78257
Shannon Brooks <sbrooks0084@gmail.com>
2022-09-29 11:35:45 -0500

    deleted ClearButton and SinglePost file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import Loading from './Components/Loading'
-import SinglePost from './Components/SinglePost'
-import Posts from './Components/Posts.js'
+import Loading from "./Components/Loading"
+//import SinglePost from "./Components/SinglePost"
+import Posts from "./Components/Posts"
 import axios from 'axios'
 // your components should be app, posts, postItem, and loading
 
@@ -15,20 +15,20 @@ const App = () => {
             const info = await axios.get('https://jsonplaceholder.typicode.com/posts')
             setPosts(info.data)
             setLoading(false)
-            //console.log(info.data)
+            console.log(info.data)
         }
         getPosts()
     }, [])
     const handleSetSinglePost = (obj) => {
         setSinglePost(obj)
     }
-    const clearSinglePost = () => {
-        setSinglePost(null)
-    }
+    // const clearSinglePost = () => {
+    //     setSinglePost(null)
+    // }
     if(!loading && !singlePost) {
         return <Posts postsProp={posts} handleSetSinglePost={handleSetSinglePost} />
-    }else if(!loading) {
-        return <SinglePost singlePost={singlePost} clearSinglePost={clearSinglePost}/>
+    // }else if(!loading) {
+    //     return <SinglePost singlePost={singlePost} clearSinglePost={clearSinglePost}/>
     }else {
         return <Loading />
     }
--- src/Components/PostItem.js
+++ src/Components/PostItem.js
@@ -4,7 +4,7 @@ const PostItem = ({postsProp, handleSetSinglePost}) => {
     const handleClick = async (e) => {
         const info = await axios.get (`https://jsonplaceholder.typicode.com/posts/${e.target.id}`)
         handleSetSinglePost(info.data)
-        console.log(e.target.data)
+        console.log(e.target.id)
     }
     return(
         <h1 onClick={handleClick} id={postsProp.id}>{postsProp.title}</h1>
